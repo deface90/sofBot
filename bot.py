f78e1cf029ef76ad6c6b67b8640df37f4707006a
deface <deface@MacBook-Pro-deface.local>
2015-09-30 16:05:01 +0500

    работа со stackoverflow

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import telegram
+import telegram, stackexchange
 import MySQLdb
 from settings import *
 
@@ -8,33 +8,59 @@ bot = telegram.Bot(token=token)
 db = MySQLdb.connect(host=mysqlHost, user=mysqlUser, passwd=mysqlPassword, db=mysqlDBName, charset='utf8')
 cursor = db.cursor()
 
-print 'Get last update id... '
-sql = """SELECT updateId FROM messages ORDER BY updateId DESC LIMIT 1"""
-try:
-    cursor.execute(sql)
-    row = cursor.fetchone()
-    if row:
-        LAST_UPDATE_ID = row[0]
-    else:
-        LAST_UPDATE_ID = None
-    print 'OK! Its ' + str(LAST_UPDATE_ID)
-except MySQLdb.Error, e:
-    print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
-    LAST_UPDATE_ID = None
 
-print 'Get telegram updates... '
-updates = bot.getUpdates(offset=LAST_UPDATE_ID+1)
-print 'OK! fetched ' + str(len(updates)) + ' update(s)! Obtain response... '
+def obtain_question(q):
+    result = False
+    so = stackexchange.Site(stackexchange.StackOverflow)
+    so_qs = so.search(intitle=q, sort='votes')[:1]
+    for so_q in so_qs:
+        print('%8d %s %s' % (so_q.id, so_q.title, so_q.url))
+        answers = so.answers(question_id=so_q.id, sort='votes')[:1]
+        if answers:
+            result = answers[0].body
 
-for upd in updates:
-    question = upd.message.text
-    sql = u"""INSERT INTO messages (updateId, chatId, userId, content) VALUES ({0}, {1}, {2}, '{3}')
-          """.format(upd.update_id, upd.message.chat.id, upd.message.from_user.id, question)
+    return result
+
+
+def run_bot():
+    print 'Get last update id... '
+    sql = """SELECT updateId FROM messages ORDER BY updateId DESC LIMIT 1"""
     try:
         cursor.execute(sql)
-        db.commit()
+        row = cursor.fetchone()
+        if row:
+            last_update_id = row[0]
+        else:
+            last_update_id = None
+        print 'OK! Its ' + str(last_update_id)
     except MySQLdb.Error, e:
         print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+        last_update_id = None
 
-db.close()
-print 'OK!'
+    print 'Get telegram updates... '
+    updates = bot.getUpdates(offset=last_update_id+1)
+    print 'OK! fetched ' + str(len(updates)) + ' update(s)! Obtain response... '
+
+    for upd in updates:
+        question = upd.message.text
+        sql = u"""INSERT INTO messages (updateId, chatId, userId, content) VALUES ({0}, {1}, {2}, '{3}')
+              """.format(upd.update_id, upd.message.chat.id, upd.message.from_user.id, question)
+        try:
+            cursor.execute(sql)
+            db.commit()
+        except MySQLdb.Error, e:
+            print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+
+        answer = obtain_question(question)
+        if answer:
+            message = answer
+        else:
+            message = 'Ниче не нашёл :('
+
+        bot.sendMessage(chat_id=upd.message.chat.id, text=message)
+
+    db.close()
+    print 'OK!'
+
+
+run_bot()
